cfd56efc330c281b9c13ef8554b656f09db0e3bd
pooneh <p.moghadda30@gmail.com>
2023-08-23 17:55:03 +0200

    successfully broke my code

--- ex16_postgreSQL/my_app/client/src/App.js
+++ ex16_postgreSQL/my_app/client/src/App.js
@@ -1,7 +1,21 @@
 
 import ListHeader from "./components/ListHeader"
+import {useEffect} from "react";
 
 const App = () => {
+    const userEmail = "test@test.com"
+    const getData = async () => {
+        try{
+            const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+            const js = await response.json()
+            console.log("wht the shell is this?");
+        }catch (err){
+            console.error(err);
+        }
+    }
+    useEffect(() =>{
+        getData();
+    }, [])
   return (
     <div className= "app">
       <ListHeader listName={"⛱ Holiday tick list"}/>
--- ex16_postgreSQL/my_app/server/server.js
+++ ex16_postgreSQL/my_app/server/server.js
@@ -2,13 +2,16 @@
 const express = require("express")
 const app = express()
 const pool = require("./db")
-
+const cors = require("cors") // if not, it will block our app due to cors policy !?!
 // get all of our todos:
 
-
+app.use(cors)
 app.get("/todos", async (req, res) =>{
+    // const email = req.params.userEmail
+    const email = 'test@test.com'
     try{
-       const todos = await pool.query('SELECT * FROM todos')
+       // const todos = await pool.query('SELECT * FROM todos')
+        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [email]) // ==show me the object of which has this user_email
         res.json(todos.rows)
     }catch (err) {
       console.log(err)
