06ef41c60dc7eb6f20137a7cb56fb3e75fe7d626
pooneh <p.moghadda30@gmail.com>
2023-08-25 21:27:09 +0200

    que

--- ex16_postgreSQL/my_app/client/src/App.js
+++ ex16_postgreSQL/my_app/client/src/App.js
@@ -1,26 +1,31 @@
 
 import ListHeader from "./components/ListHeader"
-import {useEffect} from "react";
-
+import {useEffect, useState} from "react"; // not quite sure how the hooks work though...
+import ListItem from "./components/ListItem";
 const App = () => {
     const userEmail = "test@test.com"
+    const [tasks, setTasks] = useState(null)
+
     const getData = async () => {
         try{
             const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
             const js = await response.json()
-            console.log("wht the shell is this?");
+
+            setTasks(js)
         }catch (err){
             console.error(err);
         }
     }
+    //interesting way to sort the task:
+    const sortedTasks = tasks?.sort((a,b) => new Date(a.date) - new Date(b.date))
     useEffect(() =>{
         getData();
     }, [])
   return (
     <div className= "app">
       <ListHeader listName={"⛱ Holiday tick list"}/>
+        {sortedTasks?.map((task) => <ListItem key={task.id} task={task} />)}
     </div>
   );
 }
-
 export default App;
--- ex16_postgreSQL/my_app/server/server.js
+++ ex16_postgreSQL/my_app/server/server.js
@@ -5,10 +5,10 @@ const pool = require("./db")
 const cors = require("cors") // if not, it will block our app due to cors policy !?!
 // get all of our todos:
 
-app.use(cors)
-app.get("/todos", async (req, res) =>{
-    // const email = req.params.userEmail
-    const email = 'test@test.com'
+app.use(cors())
+app.get("/todos/:userEmail", async (req, res) =>{
+    const email = req.params.userEmail
+    // const email = 'test@test.com'
     try{
        // const todos = await pool.query('SELECT * FROM todos')
         const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [email]) // ==show me the object of which has this user_email
